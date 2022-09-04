cec45ade75058d1c4ec6fa5b9f6cb43082c8780a
Michael Phelps <mpphelps@gmail.com>
2022-09-04 13:29:09 -0400

    Added memento design pattern

---
+++ DesignPatterns/DesignPatterns/Memento/Document.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Memento
+{
+    public class Document
+    {
+        private string _content;
+        private string _fontName;
+        private int _fontSize;
+
+        public DocumentMemento createMemento()
+        {
+            return new DocumentMemento(_content, _fontName, _fontSize);
+        }
+
+        public void restore(DocumentMemento memento)
+        {
+            _content = memento.GetContent();
+            _fontName = memento.GetFontName();
+            _fontSize = memento.GetFontSize();
+        }
+
+        private string GetContent()
+        {
+            return _content;
+        }
+        public void SetContent(string content)
+        {
+            _content = content;
+        }
+        public String getFontName()
+        {
+            return _fontName;
+        }
+
+        public void setFontName(String fontName)
+        {
+            _fontName = fontName;
+        }
+
+        public int getFontSize()
+        {
+            return _fontSize;
+        }
+
+        public void setFontSize(int fontSize)
+        {
+            _fontSize = fontSize;
+        }
+
+        public String ToString()
+        {
+            return "Document{" +
+                    "content='" + _content + '\'' +
+                    ", fontName='" + _fontName + '\'' +
+                    ", fontSize=" + _fontSize +
+                    '}';
+        }
+
+    }
+}
---
+++ DesignPatterns/DesignPatterns/Program.cs
@@ -0,0 +1,49 @@
+﻿using DesignPatterns.Memento;
+using System;
+
+namespace DesignPatterns
+{
+    
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var editor = new Editor();
+            var history  = new History();
+            editor.SetContent("a");
+            history.Push(editor.CreateState());
+
+            editor.SetContent("b");
+            history.Push(editor.CreateState());
+
+            editor.SetContent("c");
+            editor.Restore(history.Pop());
+            editor.Restore(history.Pop());
+
+            Console.WriteLine(editor.GetContent());
+
+            var document = new Document();
+            var documentHistory = new DocumentHistory();
+            document.SetContent("a");
+            document.setFontName("Times New Roman");
+            document.setFontSize(12);
+            documentHistory.Push(document.createMemento());
+
+            document.setFontSize(10);
+            documentHistory.Push(document.createMemento());
+
+            document.SetContent("Hello World");
+            documentHistory.Push(document.createMemento());
+
+            Console.WriteLine(document.ToString());
+            document.restore(documentHistory.Pop());
+
+            Console.WriteLine(document.ToString());
+            document.restore(documentHistory.Pop());
+
+            Console.WriteLine(document.ToString());
+
+
+        }
+    }
+}
