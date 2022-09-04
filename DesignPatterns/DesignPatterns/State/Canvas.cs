3fbd9f85b47439daca9848b6e0c1cd79e0e049c4
Michael Phelps <mpphelps@gmail.com>
2022-09-04 14:10:45 -0400

    Started state design

---
+++ DesignPatterns/DesignPatterns/State/Canvas.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.State
+{
+    public class Canvas
+    {
+        public ToolType CurrentTool { get; set; }
+
+        public void MouseDown()
+        {
+            if (CurrentTool == ToolType.SELECTION)
+                Console.WriteLine("Selection icon");
+            else if (CurrentTool == ToolType.BRUSH)
+                Console.WriteLine("Brush icon");
+            else if (CurrentTool == ToolType.ERASER)
+                Console.WriteLine("Eraser icon");
+        }
+        public void MouseUp()
+        {
+            if (CurrentTool == ToolType.SELECTION)
+                Console.WriteLine("Draw dashed rectange");
+            else if (CurrentTool == ToolType.BRUSH)
+                Console.WriteLine("Draw a line");
+            else if (CurrentTool == ToolType.ERASER)
+                Console.WriteLine("Erase something");
+        }
+    }
+}
---
+++ DesignPatterns/DesignPatterns/State/ToolType.cs
@@ -0,0 +1,6 @@
+﻿public enum ToolType
+{
+    SELECTION,
+    BRUSH,
+    ERASER
+}
